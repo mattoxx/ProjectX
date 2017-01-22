b50eb3e7267412a399ce106addc223ebefd042ac
Martin Mattoxx <mattoxx@centrum.sk>
2017-01-22 12:56:35 +0100

    some update

--- ProjectX/MainWindow.xaml.cs
+++ ProjectX/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SQLite;
 using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.IO;
+using System.Drawing;
+
 
 namespace ProjectX
 {
@@ -22,27 +26,124 @@ namespace ProjectX
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int imagePosition = 0;
+        public bool rendered = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            connectToSQLite();
+            imagePosition = 0;
+           rendered = connectToSQLite(imagePosition);
+            
         }
 
-        private void connectToSQLite()
+        private bool connectToSQLite(int imagePosition)
         {
             var conn = new SQLiteConnection(@"data source=C:\Users\Martin\Documents\Visual Studio 2015\DBSQLite\dbTest.db;Version=3;");
             conn.Open();
 
             var context = new DataContext(conn);
-            var x = "select * from Screens";
-            SQLiteCommand command = new SQLiteCommand(x, conn);
-            SQLiteDataReader reader = command.ExecuteReader();
 
             
+
+           
+
+            var y = context.GetTable<Screens>().ToList();
+
+           
+
+
+
+            var item = y;
+            
+            var cc = y.Count();
+            if (cc <= imagePosition || imagePosition < 0)
+            {
+                if (imagePosition < 0)
+                    imagePosition = 0;
+                else imagePosition--;
+                return false;
+            }
+                
+
+
+
+            //var labelContent  = y.FirstOrDefault().Desc;
+            var description = item.ElementAt<Screens>(imagePosition).Desc;
+            //var name = item.FirstOrDefault().Desc;
+            label.Content = "Image Description: "+  description;
+
+            
+
+
+           // glass.Source = new BitmapImage(new Uri("lavka.jpg",UriKind.Relative));
+
+            
+
+             
+            var imageData = item.ElementAt<Screens>(imagePosition).Screen;
+
+            //  var ms = new MemoryStream(imageData);
+            //  System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+
+            MemoryStream memoryStream = new MemoryStream(imageData);
+            
+                var imageSource = new BitmapImage();
+                imageSource.BeginInit();
+                imageSource.StreamSource = memoryStream;
+                imageSource.EndInit();
+
+                // Assign the Source property of your image
+                glass.Source = imageSource;
+
+            
+
+
+
+            
+
+        //   image.Save(@"C:\Users\Martin\Documents\Visual Studio 2015\Projects\ProjectX\ProjectX\images\newImage.jpg");
+
+            
+
+         //  glass.Source = new BitmapImage(new Uri(@"C:\Users\Martin\Documents\Visual Studio 2015\Projects\ProjectX\ProjectX\images\newImage.jpg", UriKind.Absolute));
+
+            conn.Close();
+            return true;
+
+        }
+
+
+
+        [Table]
+        public class Screens
+        {
+            [Column]
+            public int Id { get; set; }
+            [Column]
+            public byte[] Screen { get; set; }
+            [Column]
+            public string Desc { get; set; }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if(rendered)
+                imagePosition = imagePosition + 1;
+            connectToSQLite(imagePosition);
+            //imagePosition = imagePosition + 1;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (imagePosition!=0)
+                imagePosition = imagePosition - 1;
+            connectToSQLite(imagePosition);
         }
     }
 }
