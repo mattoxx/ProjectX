1bb157088683314e21f56303e00ad9955f6b20b5
Martin Mattoxx <mattoxx@centrum.sk>
2017-01-22 13:38:12 +0100

    aa

--- ProjectX/MainWindow.xaml.cs
+++ ProjectX/MainWindow.xaml.cs
@@ -17,7 +17,8 @@ using System.Data.Linq;
 using System.Data.Linq.Mapping;
 using System.IO;
 using System.Drawing;
-
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ProjectX
 {
@@ -27,7 +28,7 @@ namespace ProjectX
     public partial class MainWindow : Window
     {
         public int imagePosition = 0;
-        public bool rendered = false;
+        
 
         public MainWindow()
         {
@@ -38,103 +39,119 @@ namespace ProjectX
         private void button_Click(object sender, RoutedEventArgs e)
         {
             imagePosition = 0;
-           rendered = connectToSQLite(imagePosition);
+           connectToSQLite(imagePosition);
             
         }
 
-        private bool connectToSQLite(int imagePosition)
+        private void connectToSQLite(int imagePosition)
         {
-            var conn = new SQLiteConnection(@"data source=C:\Users\Martin\Documents\Visual Studio 2015\DBSQLite\dbTest.db;Version=3;");
-            conn.Open();
+           // var conn = new SQLiteConnection(@"data source=C:\Users\Martin\Documents\Visual Studio 2015\DBSQLite\dbTest.db;Version=3;");
+            //conn.Open();
 
-            var context = new DataContext(conn);
+            
+            
+
+           
 
             
 
            
 
-            var y = context.GetTable<Screens>().ToList();
+        //    var y = context.GetTable<Screens>().ToList();
 
            
 
 
 
-            var item = y;
+        //    var item = y;
             
-            var cc = y.Count();
-            if (cc <= imagePosition || imagePosition < 0)
-            {
-                if (imagePosition < 0)
-                    imagePosition = 0;
-                else imagePosition--;
-                return false;
-            }
+        //    var cc = y.Count();
+        //    if (cc <= imagePosition || imagePosition < 0)
+        //    {
+        //        if (imagePosition < 0)
+        //            imagePosition = 0;
+        //        else imagePosition--;
+        //        return false;
+        //    }
                 
 
 
 
-            //var labelContent  = y.FirstOrDefault().Desc;
-            var description = item.ElementAt<Screens>(imagePosition).Desc;
-            //var name = item.FirstOrDefault().Desc;
-            label.Content = "Image Description: "+  description;
+        //    //var labelContent  = y.FirstOrDefault().Desc;
+        //    var description = item.ElementAt<Screens>(imagePosition).Desc;
+        //    //var name = item.FirstOrDefault().Desc;
+        //    label.Content = "Image Description: "+  description;
 
             
 
 
-           // glass.Source = new BitmapImage(new Uri("lavka.jpg",UriKind.Relative));
+        //   // glass.Source = new BitmapImage(new Uri("lavka.jpg",UriKind.Relative));
 
             
 
              
-            var imageData = item.ElementAt<Screens>(imagePosition).Screen;
+        //    var imageData = item.ElementAt<Screens>(imagePosition).Pic;
 
-            //  var ms = new MemoryStream(imageData);
-            //  System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+        //    //  var ms = new MemoryStream(imageData);
+        //    //  System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
 
-            MemoryStream memoryStream = new MemoryStream(imageData);
+        //    MemoryStream memoryStream = new MemoryStream(imageData);
             
-                var imageSource = new BitmapImage();
-                imageSource.BeginInit();
-                imageSource.StreamSource = memoryStream;
-                imageSource.EndInit();
+        //        var imageSource = new BitmapImage();
+        //        imageSource.BeginInit();
+        //        imageSource.StreamSource = memoryStream;
+        //        imageSource.EndInit();
 
-                // Assign the Source property of your image
-                glass.Source = imageSource;
-
-            
-
-
+        //        // Assign the Source property of your image
+        //        glass.Source = imageSource;
 
             
 
-        //   image.Save(@"C:\Users\Martin\Documents\Visual Studio 2015\Projects\ProjectX\ProjectX\images\newImage.jpg");
+
 
             
 
-         //  glass.Source = new BitmapImage(new Uri(@"C:\Users\Martin\Documents\Visual Studio 2015\Projects\ProjectX\ProjectX\images\newImage.jpg", UriKind.Absolute));
+        ////   image.Save(@"C:\Users\Martin\Documents\Visual Studio 2015\Projects\ProjectX\ProjectX\images\newImage.jpg");
 
-            conn.Close();
-            return true;
+            
+
+        // //  glass.Source = new BitmapImage(new Uri(@"C:\Users\Martin\Documents\Visual Studio 2015\Projects\ProjectX\ProjectX\images\newImage.jpg", UriKind.Absolute));
+
+        //    conn.Close();
+        //    return true;
 
         }
 
 
 
-        //[Table]
-        //public class Screens
-        //{
-        //    [Column]
-        //    public int Id { get; set; }
-        //    [Column]
-        //    public byte[] Screen { get; set; }
-        //    [Column]
-        //    public string Desc { get; set; }
-        //}
+       // [Table]
+        public class Screen
+        {
+            //[Column]
+            public int Id { get; set; }
+            //[Column]
+            public byte[] Pic { get; set; }
+            //[Column]
+            public string Desc { get; set; }
+        }
+
+
+        public class DbTest : DbContext
+        {
+            public DbTest()
+            {
+                Database.SetInitializer<DbTest>(null);
+            }
+
+            public virtual DbSet<Screen> Screens { get; set; }
+
+           
+        }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            if(rendered)
-                imagePosition = imagePosition + 1;
+            
+            imagePosition = imagePosition + 1;
             connectToSQLite(imagePosition);
             //imagePosition = imagePosition + 1;
         }
